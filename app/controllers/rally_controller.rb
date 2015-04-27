a090ef5ade21115c1ec116b42c7f629bc8d61126
pedroortizaguirre <pedroortizaguirre@gmail.com>
2015-04-27 13:24:14 -0700

    Plan Estimates

--- app/controllers/rally_controller.rb
+++ app/controllers/rally_controller.rb
@@ -8,19 +8,21 @@
 			@projects=Rally.display_projects(params[:id])
 		end
 
-		def show_projects
-			@projects=Rally.display_projects(params[:name])
-		end
-
 		def details		
 			@iterations=Rally.display_iterations(params[:id])		
-			# @user_stories= Rally.display_user_stories(params[:id])
+			@user_stories= Rally.display_user_stories(params[:id])	
+			@bugs= Rally.display_iteration_bugs(params[:id])		
 			
-			puts "#{@iterations.inspect}<<<<<<<<<<<"
-		end
+			@PlanEstimate= 0.0
+			@TaskEstimateTotal= 0.0
+			@TaskActualTotal= 0.0
 
-		def show_user_stories 
-			@user_stories= Rally.display_user_stories(params[:id])
+			@user_stories.each do |i|
+				@PlanEstimate = @PlanEstimate + i.PlanEstimate.to_f
+				@TaskEstimateTotal= @TaskEstimateTotal + i.TaskEstimateTotal.to_f
+				@TaskActualTotal= @TaskActualTotal + i.TaskActualTotal.to_f
+				puts "#{@PlanEstimate.inspect}<<"
+			end
 		end
 
 		def show_bugs
--- app/models/rally.rb
+++ app/models/rally.rb
@@ -2,59 +2,52 @@ class Rally < ActiveRecord::Base
 	
 	headers = RallyAPI::CustomHttpHeader.new({:vendor => "Vendor", :name => "MindGT Admin", :version => "1.0"})
 	@config = {:base_url => "https://rally1.rallydev.com/slm"}
-	@config[:api_key]   = "_01s48oJoTiqbNYHd0FyBeBu7OKFF0aGruXSBT21GOQ"
+	@config[:api_key]   = "_zPObD41eQb2Gyow8xgae0w1Jik3M0ychJKnCwSzIBU"
 	@config[:workspace] = "Internal Development Group"
-	@config[:project]    = "MindGT Admin"
+	#@config[:project] = "MindGT Admin"
 	@config[:headers]    = headers 
-	
-	# def to_param
- #  		name
-	# end
+
 
 	def self.display_workspace
- 			rally = RallyAPI::RallyRestJson.new(@config)
- 			defect_query = RallyAPI::RallyQuery.new()
- 			defect_query.type = "Workspace"
- 			defect_query.fetch = "Name,Projects,FormattedID,ObjectID"
- 			defect_query.project_scope_up = false
- 			defect_query.project_scope_down = true 
- 			 			
- 			workspace = rally.find(defect_query)
+		rally = RallyAPI::RallyRestJson.new(@config)
+		defect_query = RallyAPI::RallyQuery.new()
+		defect_query.type = "Workspace"
+		defect_query.fetch = "Name,Projects,FormattedID,ObjectID"
+		defect_query.project_scope_up = false
+		defect_query.project_scope_down = true 
 
- 	end
- 		def self.display_projects(id)
- 			rally = RallyAPI::RallyRestJson.new(@config)
- 			defect_query = RallyAPI::RallyQuery.new() 			
-  			defect_query.query_string ="(ObjectID = #{id})"
- 			defect_query.type = "Projects"
- 			defect_query.fetch = "Name,FormattedID,ObjectID,Iterations"
- 			defect_query.project_scope_up = false
- 			defect_query.project_scope_down = true 
- 			 			
- 			projects = rally.find(defect_query)
- 	end
+		workspace = rally.find(defect_query)
+
+	end
+
+	def self.display_projects(id)
+		rally = RallyAPI::RallyRestJson.new(@config)
+		defect_query = RallyAPI::RallyQuery.new() 			
+		defect_query.query_string ="(ObjectID = #{id})"
+		defect_query.type = "Projects"
+		defect_query.fetch = "Name,FormattedID,ObjectID,Iterations"
+		defect_query.project_scope_up = false
+		defect_query.project_scope_down = true 
+
+		projects = rally.find(defect_query)
+	end
 
 	def self.display_iterations(id)
 		rally = RallyAPI::RallyRestJson.new(@config)
 		defect_query = RallyAPI::RallyQuery.new()
 		defect_query.query_string ="(ObjectID = #{id})"
 		defect_query.type = "Iteration"
-		defect_query.fetch = "ObjectID,EndDate,Name,ScheduleState,PlanEstimate,Iteration,Project,TaskActualTotal,Owner,TaskEstimateTotal,Tasks"
-  			#defect_query.limit      = 10          #optional - default is 99999
- 			# defect_query.page_size  = 10
- 			defect_query.project_scope_up = false
- 			defect_query.project_scope_down = true
-  			#defect_query.order = "FormattedID Desc"
-  			results = rally.find(defect_query)
+		defect_query.fetch = "ObjectID,EndDate,Name,State,PlannedVelocity,Project,StartDate,PlanEstimate"
+ 		iterations = rally.find(defect_query)
+ 	end 
 
-  	end 
-
-  		def self.display_user_stories(id)
-  			rally = RallyAPI::RallyRestJson.new(@config)
-  			defect_query = RallyAPI::RallyQuery.new()
-  			defect_query.query_string ="(Iteration.ObjectID = #{id})"
-  			defect_query.type = "hierarchicalrequirement"
-  			defect_query.fetch = "Name,FormattedID,ScheduleState,PlanEstimate,Iteration,TaskActualTotal,Owner,TaskEstimateTotal,Tasks"
+ 	def self.display_user_stories(id)
+ 		rally = RallyAPI::RallyRestJson.new(@config)
+ 		defect_query = RallyAPI::RallyQuery.new()
+ 		defect_query.query_string ="(Iteration.ObjectID = #{id})"
+ 		defect_query.type = "hierarchicalrequirement"
+ 		defect_query.fetch = "Name,FormattedID,PlanEstimate,TaskActualTotal,TaskEstimateTotal,Owner,ScheduleState"
+  			
   			#defect_query.limit      = 10          #optional - default is 99999
  			# defect_query.page_size  = 10
  			defect_query.project_scope_up = false
